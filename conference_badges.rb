6827fa6b7fc4f5b4b74291bde7def82fdfd510f5
Nancy Jackson <nbjackson23@gmail.com>
2020-08-05 11:23:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,9 @@ end
 
 
 def batch_badge_creator(array)
-  array = []
+  nuarray = []
   array.each do |name|
-    array.push("Hello, my name is #{name}.")
+    nuarray.push("Hello, my name is #{name}.")
   end
   return array
 end
